e381855cededcb4c67d1650fc786d8725698f067
umblox <39423646+umblox@users.noreply.github.com>
2026-06-23 20:07:51 +0700

    Create AppDatabase for local Room database setup

---
+++ data/local/database/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.inventorypos.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.inventorypos.data.local.dao.*
+import com.inventorypos.data.local.entity.*
+
+@Database(
+    entities = [
+        ProductEntity::class,
+        CategoryEntity::class,
+        UserEntity::class,
+        TransactionEntity::class,
+        TransactionItemEntity::class,
+        CustomerEntity::class,
+        StockLogEntity::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun productDao(): ProductDao
+    abstract fun categoryDao(): CategoryDao
+    abstract fun userDao(): UserDao
+    abstract fun transactionDao(): TransactionDao
+    abstract fun customerDao(): CustomerDao
+    abstract fun stockDao(): StockDao
+}
