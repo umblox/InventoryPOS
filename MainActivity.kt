827cdb72c562418c7faed6b8099babaa78880c65
umblox <39423646+umblox@users.noreply.github.com>
2026-06-23 20:01:10 +0700

    Add MainActivity with splash screen and navigation

---
+++ MainActivity.kt
@@ -0,0 +1,38 @@
+package com.inventorypos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.inventorypos.presentation.navigation.AppNavigation
+import com.inventorypos.presentation.theme.InventoryPOSTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        
+        splashScreen.setKeepOnScreenCondition { false }
+        enableEdgeToEdge()
+        
+        setContent {
+            InventoryPOSTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    AppNavigation(navController = navController)
+                }
+            }
+        }
+    }
+}
