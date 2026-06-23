c6efc75dffc8cf691e377deaa0e8e716c7f17883
umblox <39423646+umblox@users.noreply.github.com>
2026-06-23 21:58:26 +0700

    Add ReportsViewModel for report management

---
+++ presentation/screens/reports/ReportsViewModel.kt
@@ -0,0 +1,8 @@
+package com.inventorypos.presentation.screens.reports
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ReportsViewModel @Inject constructor() : ViewModel()
