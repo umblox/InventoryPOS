b767efe32d8ae0d1577eeabb1f5921dface0e4c3
umblox <39423646+umblox@users.noreply.github.com>
2026-06-24 07:36:00 +0700

    Update AppModule.kt

--- app/src/main/java/com/inventorypos/di/AppModule.kt
+++ app/src/main/java/com/inventorypos/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.inventorypos.di
 
+import com.inventorypos.data.local.dao.CategoryDao // Pastikan ini ada
 import com.inventorypos.data.local.dao.ProductDao
+import com.inventorypos.domain.repository.CategoryRepository // Pastikan ini ada
+import com.inventorypos.domain.repository.CategoryRepositoryImpl // Pastikan ini ada
 import com.inventorypos.domain.repository.ProductRepository
 import com.inventorypos.domain.repository.ProductRepositoryImpl
 import dagger.Module
@@ -19,5 +22,14 @@ object AppModule {
         productDao: ProductDao
     ): ProductRepository {
         return ProductRepositoryImpl(productDao)
+    }
+
+    // TAMBAHKAN BLOK INI:
+    @Provides
+    @Singleton
+    fun provideCategoryRepository(
+        categoryDao: CategoryDao
+    ): CategoryRepository {
+        return CategoryRepositoryImpl(categoryDao)
     }
 }
