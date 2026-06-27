4cde8656f36572c7637a494d3d74ea8af86fdb3d
umblox <39423646+umblox@users.noreply.github.com>
2026-06-27 18:10:53 +0700

    Update AuthRepository.kt

--- app/src/main/java/com/inventorypos/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/inventorypos/domain/repository/AuthRepository.kt
@@ -1,7 +1,11 @@
+package com.inventorypos.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
 interface AuthRepository {
     suspend fun login(username: String, password: String): Result<Unit>
     suspend fun register(fullName: String, username: String, password: String): Result<Unit>
-    suspend fun resetPassword(username: String, newPassword: String): Result<Unit> // Tambahkan ini
+    suspend fun resetPassword(username: String, newPassword: String): Result<Unit>
     suspend fun logout()
     fun isUserLoggedIn(): Flow<Boolean>
 }
