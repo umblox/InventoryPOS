f063aeb6cddfe3d22cc298f7a72e0b55026cc029
umblox <39423646+umblox@users.noreply.github.com>
2026-06-27 07:05:52 +0700

    Update Theme.kt

--- app/src/main/java/com/inventorypos/presentation/theme/Theme.kt
+++ app/src/main/java/com/inventorypos/presentation/theme/Theme.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.compose.ui.graphics.Color
 
 private val PremiumDarkColorScheme = darkColorScheme(
     primary = PremiumGold,
