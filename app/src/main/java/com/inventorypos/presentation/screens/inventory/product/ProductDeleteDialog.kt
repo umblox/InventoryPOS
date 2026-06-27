5764b23b96971ab1beda99d56032bf556efaeb67
umblox <39423646+umblox@users.noreply.github.com>
2026-06-27 07:59:01 +0700

    Add import for Screen navigation in ProductDeleteDialog

--- app/src/main/java/com/inventorypos/presentation/screens/inventory/product/ProductDeleteDialog.kt
+++ app/src/main/java/com/inventorypos/presentation/screens/inventory/product/ProductDeleteDialog.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.inventorypos.presentation.components.common.CustomButton
+import com.inventorypos.presentation.navigation.Screen
 import com.inventorypos.presentation.theme.*
 
 @Composable
