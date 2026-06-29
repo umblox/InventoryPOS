fad5cdde37e0fa21c90dffb4f495ac32237933bc
umblox <39423646+umblox@users.noreply.github.com>
2026-06-29 16:06:40 +0700

    Create SupplierWithPrice.kt

---
+++ app/src/main/java/com/inventorypos/data/local/entity/SupplierWithPrice.kt
@@ -0,0 +1,11 @@
+package com.inventorypos.data.local.entity
+
+import androidx.room.Embedded
+
+// Ini bukan tabel, tapi wadah untuk menampung hasil JOIN
+data class SupplierWithPrice(
+    @Embedded val supplier: SupplierEntity,
+    val buyPrice: Double,
+    val isPrimary: Boolean
+)
+
