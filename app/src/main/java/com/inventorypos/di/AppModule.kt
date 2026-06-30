ea6a3da46a255037c44fcf617b82d05bbd2bf7b3
umblox <39423646+umblox@users.noreply.github.com>
2026-06-30 12:43:53 +0700

    Clean up AppModule by removing commented code
    Removed unused DAO and repository provider methods from AppModule.

--- app/src/main/java/com/inventorypos/di/AppModule.kt
+++ app/src/main/java/com/inventorypos/di/AppModule.kt
@@ -6,40 +6,16 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-// IMPORT DAO
 import com.inventorypos.data.local.dao.*
-// IMPORT DATABASE
 import com.inventorypos.data.local.database.AppDatabase
-
-// IMPORT LAINNYA
 import com.inventorypos.data.preferences.AuthPreferences
-
-// IMPORT DOMAIN (Interface)
 import com.inventorypos.domain.repository.*
-
-// IMPORT DATA (Implementasi)
 import com.inventorypos.data.repository.*
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    
-    // 1. PROVIDE DAOs (Dibutuhkan untuk membuat Repository)
-    // ⬇️ BISA DIHAPUS karena sudah ada di DatabaseModule.kt
-    // @Provides
-    // @Singleton
-    // fun provideProductSupplierDao(database: AppDatabase): ProductSupplierDao {
-    //     return database.productSupplierDao()
-    // }
 
-    // @Provides
-    // @Singleton
-    // fun provideSupplierDao(database: AppDatabase): SupplierDao {
-    //     return database.supplierDao()
-    // }
-
-    // 2. PROVIDE REPOSITORIES
-    
     @Provides
     @Singleton
     fun provideSupplierRepository(supplierDao: SupplierDao): SupplierRepository {
@@ -67,7 +43,6 @@ object AppModule {
         return AuthRepositoryImpl(userDao, authPreferences)
     }
 
-    // ➕ TAMBAHAN: TransactionRepository (untuk POS/checkout)
     @Provides
     @Singleton
     fun provideTransactionRepository(
@@ -78,14 +53,12 @@ object AppModule {
         return TransactionRepositoryImpl(transactionDao, productDao, stockDao)
     }
 
-    // ➕ TAMBAHAN: UserRepository (untuk user management)
     @Provides
     @Singleton
     fun provideUserRepository(userDao: UserDao): UserRepository {
         return UserRepositoryImpl(userDao)
     }
 
-    // ➕ TAMBAHAN: CustomerRepository (untuk customer management)
     @Provides
     @Singleton
     fun provideCustomerRepository(customerDao: CustomerDao): CustomerRepository {
