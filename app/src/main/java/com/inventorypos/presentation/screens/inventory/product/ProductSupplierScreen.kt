974d2136e766e65b6390be139fc6279e1982576f
umblox <39423646+umblox@users.noreply.github.com>
2026-07-01 07:18:40 +0700

    Add import for background in ProductSupplierScreen

--- app/src/main/java/com/inventorypos/presentation/screens/inventory/product/ProductSupplierScreen.kt
+++ app/src/main/java/com/inventorypos/presentation/screens/inventory/product/ProductSupplierScreen.kt
@@ -1,5 +1,6 @@
 package com.inventorypos.presentation.screens.inventory.product
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
